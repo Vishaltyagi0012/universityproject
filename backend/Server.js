9ba5bf4f7f578dca27e71362c5e23352fe86c0c3
Vishal <vishal.tyagi19062001@gmail.com>
2025-07-02 13:51:19 +0530

    tokenauth

--- backend/Server.js
+++ backend/Server.js
@@ -8,6 +8,9 @@ import cors from 'cors';
 import '../backend/db/config.js';
 import users from '../backend/db/users.js';
 import contact from '../backend/db/contact.js';
+import Jwt from 'jsonwebtoken';
+const jwtkey = 'univer';
+
 const app = express();
 
 app.use(express.json());
@@ -18,7 +21,13 @@ app.post("/register", async (req, resp) => {
     let result = await User.save();
     result = result.toObject();
     delete result.password
-    resp.send(result);
+
+    Jwt.sign({ result }, jwtkey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+            resp.send({ result: 'something went wrong, Please try after sometime' })
+        }
+        resp.send({ result, auth: token })
+    })
 })
 
 app.post("/login", async (req, resp) => {
@@ -26,7 +35,13 @@ app.post("/login", async (req, resp) => {
     if (req.body.password && req.body.email) {
         let user = await users.findOne(req.body).select("-password");
         if (user) {
-            resp.send(user)
+            Jwt.sign({ user }, jwtkey, { expiresIn: "2h" }, (err, token) => {
+                if (err) {
+                    resp.send({ result: 'something went wrong, Please try after sometime' })
+                }
+                resp.send({ user, auth: token })
+            })
+
         } else {
             resp.send({ result: 'NO user found' })
         }
